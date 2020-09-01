60a40d9670d38c1f0a2462b746985cae1efa60a3
Daniil Orlov <dnl.orlov@gmail.com>
2020-09-02 00:00:49 +0300

    timer is working now

--- app/src/main/java/orlov/daniil/timerfordolcegustomachines/DetailedCoffeeActivity.java
+++ app/src/main/java/orlov/daniil/timerfordolcegustomachines/DetailedCoffeeActivity.java
@@ -1,39 +1,41 @@
 package orlov.daniil.timerfordolcegustomachines;
 
-import androidx.annotation.MainThread;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
-import java.util.Timer;
 
-import io.reactivex.Flowable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 import orlov.daniil.timerfordolcegustomachines.data.Capsule;
 import orlov.daniil.timerfordolcegustomachines.data.CapsuleDao;
 import orlov.daniil.timerfordolcegustomachines.data.CoffeeDatabase;
 
 public class DetailedCoffeeActivity extends AppCompatActivity {
 
-    Disposable disposable;
-
+    private Disposable disposable;
+    private CountDownTimer timer;
+    private TimerStatus timerStatus;
     private int brewId = -1;
     private String brewName;
     private String brewColor;
     private boolean doubleCap;
     private List<Capsule> capsuleList;
+    private int firstCapBrewTime;
+    private int secondCapBrewTime;
 
     private View linearSingleCap;
     private View linearDoubleCap;
@@ -47,6 +49,7 @@ public class DetailedCoffeeActivity extends AppCompatActivity {
     private TextView doubleSecondBrewType;
     private TextView doubleSecondCapDescription;
     private TextView doubleSecondVol;
+    private TextView viewTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +58,8 @@ public class DetailedCoffeeActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
 
+        timerStatus = TimerStatus.NOT_ACTIVE;
+        viewTimer = findViewById(R.id.timerView);
         getExtras();
         getViews();
         getCapsuleData();
@@ -80,7 +85,7 @@ public class DetailedCoffeeActivity extends AppCompatActivity {
                 .subscribe(new Consumer<List<Capsule>>() {
                     @Override
                     public void accept(List<Capsule> capsules) throws Exception {
-                        setViews(capsules);
+                        setCapsuleData(capsules);
                     }
                 });
     }
@@ -105,12 +110,14 @@ public class DetailedCoffeeActivity extends AppCompatActivity {
         }
     }
 
-    private void setViews(List<Capsule> capsules) {
+    private void setCapsuleData(List<Capsule> capsules) {
         if (doubleCap) {
             linearSingleCap.setVisibility(View.GONE);
             linearDoubleCap.setVisibility(View.VISIBLE);
             Capsule capsule1 = capsules.get(0);
             Capsule capsule2 = capsules.get(1);
+            firstCapBrewTime = capsule1.brewTime;
+            secondCapBrewTime = capsule2.brewTime;
             doubleFirstVol.setText(capsule1.volume + getString(R.string.cap_volume));
             doubleSecondVol.setText(capsule2.volume + getString(R.string.cap_volume));
             doubleFirstCapDescription.setText(capsule1.capsuleType);
@@ -126,11 +133,85 @@ public class DetailedCoffeeActivity extends AppCompatActivity {
         } else {
             linearDoubleCap.setVisibility(View.GONE);
             linearSingleCap.setVisibility(View.VISIBLE);
-            String capVolume = capsules.get(0).volume + getString(R.string.cap_volume);
+            Capsule capsule = capsules.get(0);
+            firstCapBrewTime = capsule.brewTime;
+            String capVolume = capsule.volume + getString(R.string.cap_volume);
             if (brewId >= 0) singeCapVol.setText(capVolume);
             if (brewName != null) singleCapBrew.setText(brewName);
             if (brewColor != null) firstCapStart.setBackgroundColor(Color.parseColor(brewColor));
         }
+    }
+
+    public void onSingleCupStartClick(View v) {
+        switch (timerStatus) {
+            case NOT_ACTIVE:
+                timerStatus = TimerStatus.SINGLE_CUP_ACTIVE;
+                startTimer(firstCapBrewTime, firstCapStart);
+                break;
+            case SINGLE_CUP_ACTIVE:
+                cancelTimer(firstCapStart);
+                break;
+            default:
+                Toast.makeText(this, getString(R.string.toast_one_cup),
+                        Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onDoubleFirstStartClick(View v) {
+        switch (timerStatus) {
+            case NOT_ACTIVE:
+                timerStatus = TimerStatus.FIRST_CUP_ACTIVE;
+                startTimer(firstCapBrewTime, firstCapStart);
+                break;
+            case FIRST_CUP_ACTIVE:
+                cancelTimer(firstCapStart);
+                break;
+            default:
+                Toast.makeText(this, getString(R.string.toast_one_cup),
+                        Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onDoubleSecondStartClick(View v) {
+        switch (timerStatus) {
+            case NOT_ACTIVE:
+                timerStatus = TimerStatus.SECOND_CUP_ACTIVE;
+                startTimer(secondCapBrewTime, secondCapStart);
+                break;
+            case SECOND_CUP_ACTIVE:
+                cancelTimer(secondCapStart);
+                break;
+            default:
+                Toast.makeText(this, getString(R.string.toast_one_cup),
+                        Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*
+     * timer for coffee brewing, displays time till end of brewing
+     * @param brewTimeForTimer brew time set by selected button
+     */
+    public void startTimer(int brewTimeForTimer, final ImageButton button) {
+        button.setImageResource(R.drawable.stop);
+        timer = new CountDownTimer(brewTimeForTimer * 1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                viewTimer.setText(Long.toString(millisUntilFinished / 1000));
+            }
+            public void onFinish() {
+                timerStatus = TimerStatus.NOT_ACTIVE;
+                viewTimer.setText("0");
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                vibrator.vibrate(500);
+                button.setImageResource(R.drawable.play);
+            }
+        }.start();
+    }
+
+    private void cancelTimer(ImageButton button){
+        timer.cancel();
+        timerStatus = TimerStatus.NOT_ACTIVE;
+        button.setImageResource(R.drawable.play);
+        viewTimer.setText("00");
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -152,5 +233,9 @@ public class DetailedCoffeeActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         disposable.dispose();
+    }
+
+    private enum TimerStatus {
+        SINGLE_CUP_ACTIVE, FIRST_CUP_ACTIVE, SECOND_CUP_ACTIVE, NOT_ACTIVE
     }
 }
